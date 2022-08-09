427463562b9f5366d0970fef7097763faf719abf
asfiowilma <asfiolithaw@yahoo.co.id>
2022-08-09 17:53:40 +0700

    ✨ add custom TextAreaField

---
+++ web/src/components/Form/TextAreaField.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { RakInputProps } from 'types/form'
+
+import { FieldError, Label, TextAreaField } from '@redwoodjs/forms'
+
+const RakTextAreaField = ({
+  name,
+  label,
+  validation,
+  defaultValue,
+  cols,
+  rows,
+}: RakInputProps) => {
+  return (
+    <>
+      {label && (
+        <Label
+          name={name}
+          className="rw-label"
+          errorClassName="rw-label rw-label-error"
+        >
+          {label}
+        </Label>
+      )}
+      <TextAreaField
+        className="rw-input"
+        errorClassName="rw-input rw-input-error"
+        {...{ name, defaultValue, validation, cols, rows }}
+      />
+      <FieldError name={name} className="rw-field-error" />
+    </>
+  )
+}
+
+export default RakTextAreaField
